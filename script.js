54beb73aed7aa1d6146041558c3b1cc355e3d858
hamzahthefantastic <118228180+hamzahthefantastic@users.noreply.github.com>
2023-01-12 22:06:26 -0500

    added car character for letters, number and special characters

--- script.js
+++ script.js
@@ -2,57 +2,40 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  // Set password length
   var length = prompt("Enter a password length (8-128 characters):");
   while (length < 8 || length > 128) {
     alert("Password length must be between 8 and 128 characters.");
     length = prompt("Enter a password length (8-128 characters):");
   }
 
-  // Set character types to include in password
   var lowercase = confirm("Include lowercase characters in the password?");
   var uppercase = confirm("Include uppercase characters in the password?");
   var numeric = confirm("Include numeric characters in the password?");
   var special = confirm("Include special characters in the password?");
 
-  // Validate that at least one character type is selected
-  while (!lowercase && !uppercase && !numeric && !special) {
-    alert("You must select at least one character type.");
-    lowercase = confirm("Include lowercase characters in the password?");
-    uppercase = confirm("Include uppercase characters in the password?");
-    numeric = confirm("Include numeric characters in the password?");
-    special = confirm("Include special characters in the password?");
-  }
-
-  // Generate password
   var characters = "";
   if (lowercase) {
-    characters += "abcdefghijklmnopqrstuvwxyz";
+    characters == "abcdefghijklmnopqrstuvwxyz";
   }
   if (uppercase) {
-    characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    characters == "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
   if (numeric) {
-    characters += "0123456789";
+    characters == "0123456789";
   }
   if (special) {
-    characters += "!@#$%^&*()_+-=[]{}|;':\"<>,.?/\\";
+    characters == "!@#$%^&*()_+-=[]{}|;':\"<>,.?/\\";
   }
-  var password = "";
-  for (var i = 0; i < length; i++) {
-    password += characters.charAt(Math.floor(Math.random() * characters.length));
+ 
+
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
   }
-  return password;
-}
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
